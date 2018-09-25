21c0258f8fd962edb0f8082ce8b036084220b7c9
Ryan Page <ryan.page@perspectium.com>
2018-09-25 14:46:54 -0700

    initial project checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+# Discord Bot - starter template written in typescript
+documentation forthcoming.
---
+++ bot.ts
@@ -0,0 +1,45 @@
+import Discord, { Message } from 'discord.js';
+import { EventEmitter } from 'events';
+import { prefix } from './config.json';
+import { token } from './config-private.json';
+
+const bot = new Discord.Client();
+const emitter = new EventEmitter();
+
+// TODO: fix implementation
+function getCommand(message: Message) {
+    const mArr = message.content.split(' ');
+    return mArr[0] + ' ' + mArr[1];
+}
+
+bot.on('ready', () => {
+    console.log('Bot has been started successfully!');
+});
+
+bot.on('message', (message: Message) => {
+    const isBot = message.author.bot;
+    if (isBot) return;
+
+    const startsWithPrefix = message.content.indexOf(prefix)
+    if (startsWithPrefix) return;
+
+    const command = getCommand(message);
+    emitter.emit(command, message);
+});
+
+/**
+ * COMMAND FUNCTIONS
+ */
+import test from './commands/test';
+
+/**
+ * COMMANDS
+ */
+// test command functionality
+emitter.on(prefix + 'test', test);
+emitter.on(prefix + 't', test);
+
+/**
+ * INIT
+ */
+bot.login(token);
