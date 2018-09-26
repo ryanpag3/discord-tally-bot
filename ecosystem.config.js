c38277742d95b6ebc21faffff43d4d35a5967f82
Ryan Page <ryanapage@gmail.com>
2018-09-26 08:45:10 -0700

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,33 @@
-{
+export default {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
   apps : [
     {
-      name      : "tally-bot",
-      script   : {
-        script: "./dist/bot.js"
-      },
+      name      : 'tally-bot',
+      script    : '.dist/bot.js',
       env: {
-        "COMMON_VARIABLE": "true"
+        COMMON_VARIABLE: 'true'
       },
       env_production : {
-        NODE_ENV: "production"
+        NODE_ENV: 'production'
       }
     }
   ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
   deploy : {
     production : {
-      user : "ryan",
-      host : "localhost",
-      ref  : "origin/master",
-      repo : "https://github.com/ryanpage42/discord-tally-bot.git",
-      path : "/var/opt/production/discord-tally-bot",
-      post-deploy : "npm i && tsc && pm2 reload ecosystem.config.js"
+      user : 'ryan',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'https://github.com/ryanpage42/discord-tally-bot.git',
+      path : '/var/opt/production/discord-tally-bot',
+      'post-deploy' : 'npm i && tsc && pm2 reload tally-bot'
     }
   }
 }
