12aa831a20dfb306f97372f97dfb31b9cdfa6ede
Ryan Page <ryanapage@gmail.com>
2018-09-25 23:27:03 -0700

    add and case

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   apps : [
     {
       name      : 'tally-bot',
-      script    : './dist/bot.js',
+      script    : 'tsc && ./dist/bot.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
