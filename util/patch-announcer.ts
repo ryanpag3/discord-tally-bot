dfbbfcfc61eb2712e65dc1bab8e3da34fffdf002
Ryan Page <ryanapage@gmail.com>
2019-02-02 21:52:08 -0800

    few small cleanup changes

--- util/patch-announcer.ts
+++ util/patch-announcer.ts
@@ -6,23 +6,29 @@ export default class PatchAnnouncer {
     constructor() {}
 
     static async announcePatch(msg: any) {
-        const server: any = await DB.Server.findOne({
-            where: {
-                id: msg.guild.id,
-                patchNotesEnabled: true
-            }
-        });
-        
-        if (!server) return;
-        if (server.lastPatchAnnounced === PatchAnnouncer.getCurrentVersion()) return;
+        try {
+            const server: any = await DB.Server.findOne({
+                where: {
+                    id: msg.guild.id,
+                    patchNotesEnabled: true
+                }
+            });
 
-        PatchAnnouncer.sendPatchMsg(msg.channel);
-        server.lastPatchAnnounced = PatchAnnouncer.getCurrentVersion();
-        await server.save();
+            if (!server) return;
+            if (server.lastPatchAnnounced === PatchAnnouncer.getCurrentVersion()) return;
+
+            PatchAnnouncer.sendPatchMsg(msg.channel);
+            server.lastPatchAnnounced = PatchAnnouncer.getCurrentVersion();
+            await server.save();
+        } catch (e) {
+            console.log(`Could not announce patch. Reason ${e}`);
+        }
     }
 
     static getCurrentVersion() {
-        const { version } = require(path.resolve(__dirname, '../../package.json'));
+        const {
+            version
+        } = require(path.resolve(__dirname, '../../package.json'));
         return version;
     }
 
@@ -30,8 +36,8 @@ export default class PatchAnnouncer {
         const richEmbed = {
             title: `:camera_with_flash: Tally Bot has been updated to v.${PatchAnnouncer.getCurrentVersion()} while you were gone!`,
             description: `See patch notes here: https://github.com/ryanpage42/discord-tally-bot/blob/master/CHANGELOG.md` +
-            `\nThese alerts will only trigger once per update, per server.` +
-            `\nDisable/Enable alerts with \`!tb patchnotes -off\` and \`!tb patchnotes -on\``
+                `\nThese alerts will only trigger once per update, per server.` +
+                `\nDisable/Enable alerts with \`!tb patchnotes -off\` and \`!tb patchnotes -on\``
         }
         channel.send(helper.buildRichMsg(richEmbed));
     }
