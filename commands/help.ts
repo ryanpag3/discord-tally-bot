feaaece6e5d5014d08a6de2d85c3201c86da3676
Ryan Page <ryanapage@gmail.com>
2020-01-11 17:37:52 -0800

    remove help weirdness

--- commands/help.ts
+++ commands/help.ts
@@ -7,10 +7,6 @@ export default (message: Message) => {
     const richEmbed = helper.getRichEmbed(message.author.username);
     richEmbed.setTitle(`:question: help`)
     richEmbed.setDescription(`Click [here](https://github.com/ryanpage42/discord-tally-bot/blob/master/README.md) for full documentation.`);
-    richEmbed.addField(`General Commands`, `[here](https://github.com/ryanpag3/discord-tally-bot/blob/master/README.md#General)`);
-    richEmbed.addField(`Tallies`, `[here](https://github.com/ryanpag3/discord-tally-bot/blob/master/README.md#tallies)`);
-    richEmbed.addField(`Announcements`, `[here](https://github.com/ryanpag3/discord-tally-bot/blob/master/README.md#announcements)`);
-    richEmbed.addField(`Timers`, `[here](https://github.com/ryanpag3/discord-tally-bot/blob/master/README.md#timers)`)
     helper.finalize(message);
     message.channel.send(richEmbed);
 }
