a91dee79a88dfb2c01dfba0894b9ca810717bf51
Ryan Page <ryanapage@gmail.com>
2020-03-13 07:43:30 -0700

    cleanup unused

--- util/tutorial.ts
+++
@@ -1,5 +0,0 @@
-export default class Tutorial {
-    static tutorialState = {};
-
-    
-}
--- util/user.ts
+++ util/user.ts
@@ -4,15 +4,6 @@ import logger from './logger';
 export default class UserUtil {
     static db: DB = new DB();
 
-    // static async initAll(users: any) {
-    //     const promises = [];
-    //     users.forEach(user => {
-    //         promises.push(UserUtil.init(user.id, user.tag));
-    //     });
-    //     await Promise.all(promises);
-    //     // TODO: deactivate the rest?
-    // }
-
     static async init(id: string, tag: string) {
         try {
             const user = await UserUtil.db.getUser(id);
