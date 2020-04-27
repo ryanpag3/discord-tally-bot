b2fdca715b691913fd0083f24c829ce6b2a7fd38
Ryan Page <ryanapage@gmail.com>
2020-04-27 07:27:06 -0700

    fix bug

--- bot/src/static/MsgEmojis.ts
+++ bot/src/static/MsgEmojis.ts
@@ -1,7 +1,6 @@
 import Commands from './Commands';
 
 const Emojis = {
-    [Commands.ANNOUNCE_CREATE]: ':trumpet:',
     [Commands.BUMP]: ':small_red_triangle:',
     [Commands.DUMP]: ':small_red_triangle_down:',
     [Commands.TALLY_REACTIONS]: ':flushed:'
