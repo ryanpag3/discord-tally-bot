941eb2a7f6733e73919db71c9cd3eb519261f93f
Ryan Page <ryanapage@gmail.com>
2020-04-30 07:40:09 -0700

    catch login error

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -10,9 +10,9 @@ import KeywordUtil from './util/keyword-util';
 import cmdHelper from './message/msg-helper';
 import CommandManager from './message/command-manager';
 import Counter from './util/counter';
-import logger from './util/logger'; 
+import logger from './util/logger';
 import DmManager from './message/dm-manager';
-import Env from './util/env'; 
+import Env from './util/env';
 import UserUtil from './util/user';
 import HealthCheckServer from './util/healthcheck-server';
 
@@ -23,11 +23,15 @@ class Bot {
     static topgg = Env.isProduction() ? new DBL(ConfigPrivate.dbots_token, Bot.client) : null;
     static db: DB = new DB();
     static initialReady: boolean = true;
-    
+
     static async start() {
-        await Bot.setup();
-        await Bot.client.login(ConfigPrivate.token);
-        Bot.healthcheck.start();
+        try {
+            await Bot.setup();
+            await Bot.client.login(ConfigPrivate.token);
+            Bot.healthcheck.start();
+        } catch (e) {
+            logger.error(`Bot errored while starting up.`, e);
+        }
     }
 
     static async setup() {
@@ -43,7 +47,7 @@ class Bot {
                 if (!Bot.initialReady) return;
                 setTimeout(() => Bot.startBroadcasting(), 5000);
                 CronAnnouncer.setBot({
-                    bot: Bot.client
+                    bot: Bot.client,
                 });
                 // we have to wait to init once login is complete
                 await Bot.db.initServers(Bot.client.guilds);
@@ -54,7 +58,7 @@ class Bot {
                 logger.error(`An error occured while running post-launch behavior.`, e);
             }
         });
-        
+
         Bot.client.on('message', async (message: Message) => {
             try {
                 const isBot = message.author.bot;
@@ -64,7 +68,7 @@ class Bot {
                     await UserUtil.init(message.author.id, message.author.tag);
                     return DmManager.handle(message);
                 }
-        
+
                 const startsWithPrefix = message.content.startsWith(Config.prefix);
                 if (!startsWithPrefix) {
                     KeywordUtil.bumpKeywordTallies(message);
@@ -79,13 +83,13 @@ class Bot {
                 logger.info(`Error while inbounding message: ` + e);
                 if (e.toString().includes('invalid command')) {
                     const richEmbed = {
-                        description: `Invalid command used.`
+                        description: `Invalid command used.`,
                     };
                     message.channel.send(cmdHelper.buildRichMsg(richEmbed));
                 }
             }
         });
-        
+
         Bot.client.on('guildCreate', async (guild: Guild) => {
             await guild.owner.send(`
             Thank you for adding Tally Bot to your server. :fist: If you did not add me, then
@@ -98,24 +102,24 @@ class Bot {
             I am always looking to improve the bot, please feel free to send feedback!
             `);
         });
-        
-        Bot.client.on('error', function(error) {
+
+        Bot.client.on('error', function (error) {
             logger.error(`client's WebSocket encountered a connection error`, error);
         });
-        
-        Bot.client.on('warn', function(info) {
+
+        Bot.client.on('warn', function (info) {
             logger.info(`warn: ${info}`);
         });
-        
-        Bot.client.on('reconnecting', function(info) {
+
+        Bot.client.on('reconnecting', function (info) {
             logger.info(`reconnecting`);
         });
 
-        Bot.client.on('resume', function() {
+        Bot.client.on('resume', function () {
             logger.info('bot has successfully reconnected');
         });
-        
-        Bot.client.on('disconnect', function(event) {
+
+        Bot.client.on('disconnect', function (event) {
             logger.info(`The WebSocket has closed and will no longer attempt to reconnect`);
             process.exit(1);
         });
@@ -127,20 +131,20 @@ class Bot {
         const statusGenerators = [
             () => {
                 let users = 0;
-                Bot.client.guilds.map(guild => (users += guild.members.size));
+                Bot.client.guilds.map((guild) => (users += guild.members.size));
                 Bot.client.user.setActivity(`${Bot.client.guilds.size} servers.`);
                 if (Bot.topgg) Bot.topgg.postStats(Bot.client.guilds.size);
             },
             () => {
                 let users = 0;
-                Bot.client.guilds.map(guild => (users += guild.members.size));
+                Bot.client.guilds.map((guild) => (users += guild.members.size));
                 Bot.client.user.setActivity(`${users} users.`);
             },
             () => {
                 Bot.client.user.setActivity(`!tb help`);
-            }
+            },
         ];
-    
+
         let i = 0;
         setInterval(
             () => {
