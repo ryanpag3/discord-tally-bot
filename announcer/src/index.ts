d883e3151e7c16df2fa60d0fb15463d32564a030
Ryan Page <ryanapage@gmail.com>
2020-05-14 06:15:40 -0700

    add message sweeping for announcer node

--- announcer/src/index.ts
+++ announcer/src/index.ts
@@ -8,7 +8,35 @@ import Cron from './util/cron';
 
 class AnnouncerBot {
     token: string;
-    client: Client = new Discord.Client();
+    client: Client = new Discord.Client({
+        messageCacheLifetime: 60,
+        messageSweepInterval: 5 * 60,
+        disabledEvents: [
+            'GUILD_SYNC',
+            'GUILD_UPDATE',
+            'GUILD_MEMBER_ADD',
+            'GUILD_MEMBER_REMOVE',
+            'GUILD_MEMBER_UPDATE',
+            'GUILD_INTEGRATIONS_UPDATE',
+            'GUILD_ROLE_CREATE',
+            'GUILD_ROLE_DELETE',
+            'GUILD_ROLE_UPDATE',
+            'MESSAGE_DELETE_BULK',
+            'MESSAGE_REACTION_ADD',
+            'MESSAGE_REACTION_REMOVE',
+            'MESSAGE_REACTION_REMOVE_ALL',
+            'USER_UPDATE',
+            'USER_NOTE_UPDATE',
+            'USER_SETTINGS_UPDATE',
+            'PRESENCE_UPDATE',
+            'VOICE_STATE_UPDATE',
+            'TYPING_START',
+            'VOICE_SERVER_UPDATE',
+            'RELATIONSHIP_ADD',
+            'RELATIONSHIP_REMOVE',
+            'WEBHOOKS_UPDATE'
+        ]
+    });
     announcements;
 
     constructor(token: string) {
